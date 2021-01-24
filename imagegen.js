690cc45671d929dbbd93d0abe88618865a0cbad9
JosiahFun <74677226+JosiahFun@users.noreply.github.com>
2021-01-23 22:11:29 -0500

    Add comments

--- imagegen.js
+++ imagegen.js
@@ -9,19 +9,20 @@ var texts = [
     'There was no ice cream in the freezer, nor did they have money to go to the store.',
     'This is the last random sentence I will be writing and I am going to stop mid-sent',
     'Italy is my favorite country; in fact, I plan to spend two weeks there next year.',
-];
-var text = texts[Math.floor(Math.random() * texts.length)];
-var siz = 30;
-const width = 600
+]; // list of prompts
+var text = texts[Math.floor(Math.random() * texts.length)]; // chosen prompt
+var siz = 30; // text size
+const width = 600 // canvas size
 const height = 600
 
-const canvas = createCanvas(width, height)
-const context = canvas.getContext('2d')
+const canvas = createCanvas(width, height) // create the canvas
+const context = canvas.getContext('2d') // idek tbh
 
 context.fillStyle = '#333'
-context.fillRect(0, 0, width, height)
+context.fillRect(0, 0, width, height) // draw the background
 
-var t1 = text;
+/* the code you see below is absolute garbage, but the JS canvas has no built-in way to word-wrap. so... yuck */
+var t1 = text; // very descriptive variable names
 var t2 = t1.split(' ');
 var t3 = [];
 var t4 = 0;
@@ -33,21 +34,23 @@ for (var i = 0; i < t2.length; i++) {
         t3[t3.length - 1] += '\n';
     }
 }
-text = t3.join(' ').split('\n');
+text = t3.join(' ').split('\n'); //add line breaks every 4 words
 
-context.font = 'bold ' + siz + 'pt Arial'
-context.textAlign = 'center'
-context.fillStyle = '#fff'
-var y = (height / 2) - ((text.length - 1) * 50 * .5);
-for (var i = 0; i < text.length; i++) {
-    context.fillText(text[i], 300, y + i * 50);
+context.font = 'bold ' + siz + 'pt Arial' // set the font and font size
+context.textAlign = 'center' // duh
+context.fillStyle = '#fff' // white color
+var y = (height / 2) - ((text.length - 1) * 50 * .5); // center text vertically
+
+// the native canvas text line breaking is extremely broken, so I had to make my own :(
+for (var i = 0; i < text.length; i++) { // for each line of text
+    context.fillText(text[i], 300, y + i * 50); // draw that line
 }
 
-const buffer = canvas.toBuffer('image/png')
-fs.writeFileSync('./image.png', buffer)
+const buffer = canvas.toBuffer('image/png') // convert the canvas data to a png
+fs.writeFileSync('./image.png', buffer) // save the png to the computer
 
 message.channel.send({
     files: [{
         attachment: './image.png'
     }],
-});
+}); // send a message with the png as the attachment
